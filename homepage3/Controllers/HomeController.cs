d568bbb5f3bb579f87efcbefc7502502fac966a8
sajja <sajja@LAPTOP-FNRUD43E>
2024-06-05 13:47:41 +0500

    change

---
+++ homepage3/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using homepage3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace homepage3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+        public IActionResult PressInquires()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
